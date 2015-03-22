62b227bff9ea96e37c09c7e2eddcdf4ff9c39178
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-03-22 14:51:48 +0100

    added additional functions for the issue model

--- routes/api/issue.js
+++ routes/api/issue.js
@@ -8,5 +8,41 @@ module.exports.addIssue = function(req, res) {
 			res.send(err);
 		}
 		res.json({issue: issue});
-	})
-}
+	});
+};
+
+module.exports.getAllIssues = function(req,res) {
+	Issue.find(function(err, issues) {
+		if(err) {
+			res.send(err);
+		}
+		res.json({issues: issues});
+	});
+};
+
+module.exports.getSingleIssue = function(req,res,issueId) {
+	Issue.findById(issueId, function(err, issue) {
+		if(err) {
+			res.send(err);
+		}
+		res.json({issue:issue});
+	});
+};
+
+module.exports.updateIssue = function(req,res,issueId) {
+	Issue.findByIdAndUpdate(issueId, {$set: req.body.issue}, function(err, issue) {
+		if(err) {
+			res.send(err);
+		}
+		res.json({issue:issue});
+	});
+};
+
+module.exports.deleteIssue = function(req,res,issueId) {
+	Issue.findByIdAndRemove(issueId, function(err) {
+		if(err) {
+			res.send(err);
+		}
+		res.sendStatus(200);
+	});
+};
