492a1745acc83338ef95505cbedcfb8aa2e12e0b
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-03-22 13:09:09 +0100

    Added basic files and information

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port',(process.env.PORT) || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api',function(req, res) {
+	res.send('Hello API');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Server is runnning at:' + app.get('port'));
+});
