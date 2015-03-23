6160272800c659386766b9dbd3306e2d2dff3c79
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-03-23 13:21:20 +0100

    addIssue implemented

--- client/app/controllers/issues.js
+++ client/app/controllers/issues.js
@@ -16,6 +16,17 @@ export default Ember.Controller.extend({
 		return model.filterBy('status','done');
 	}.property('model.@each.status'),
 
+	hours: [1,2,3,5,8],
+	status: ['todo','inprogress','done'],
+
+	newissue: {
+		title: '',
+		description: '',
+		work: 1,
+		user: '',
+		status: 'todo'
+	},
+
 	actions: {
 		issueDropped: function(dropZone,issueId) {
 			console.log(dropZone);
@@ -58,6 +69,28 @@ export default Ember.Controller.extend({
 				issue.destroyRecord();
 			} else {
 				debug.error("No issue with ID: " + issueId);
+			}
+		},
+		addIssue: function() {
+			if(this.get('newissue.title') !== '' && this.get('newissue.user') !== '') {
+				console.log("creating new issue");
+				var issue = this.store.createRecord('issue');
+				issue.set('title',this.get('newissue.title'));
+				issue.set('description',this.get('newissue.description'));
+				issue.set('work',this.get('newissue.work'));
+				issue.set('user',this.get('newissue.user'));
+				issue.set('status',this.get('newissue.status'));
+				console.log(issue);
+				issue.save();
+				this.set('newissue',{
+					title:'',
+					description:'',
+					work:1,
+					user:'',
+					status:'todo'
+				})
+			} else {
+				console.log('No Title or User');
 			}
 		}
 	}
