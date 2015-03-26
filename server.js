1c45bf6121edbaff4d90c5568b9a6933b2c0103d
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-03-26 18:53:57 +0100

    added route handling for client app

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 
 console.log(process.env.MONGOLAB_URI);
-if(process.env.MONGOLAB_URI === undefined) {
+if (process.env.MONGOLAB_URI === undefined) {
 	mongoose.connect('mongodb://localhost/ScrumServer');
 } else {
 	mongoose.connect(process.env.MONGOLAB_URI);
@@ -13,14 +13,14 @@ if(process.env.MONGOLAB_URI === undefined) {
 var app = express();
 var api = require('./routes/api');
 
-app.set('port',(process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin','*');
-	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Content-Length, X-Requested-With');
 
-	if('OPTIONS' == req.method) {
+	if ('OPTIONS' == req.method) {
 		res.sendStatus(200);
 	} else {
 		next();
@@ -28,11 +28,14 @@ var allowCrossDomain = function(req, res, next) {
 };
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
+app.use('/issues', express.static(__dirname + '/public')); // Need for client app. Not the cleanest way to do it
 
-app.use('/api',api);
+app.use('/api', api);
 
 /*app.get('/api',function(req, res) {
 	res.send('Hello API');
